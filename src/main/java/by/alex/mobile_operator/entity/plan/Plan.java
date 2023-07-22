352eb4f66f20cc6c014e174c7acfa07cd7e09e37
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-07-22 13:13:35 +0300

    refactor: remove @NoArgsConstructor annotation

--- src/main/java/by/alex/mobile_operator/entity/plan/Plan.java
+++ src/main/java/by/alex/mobile_operator/entity/plan/Plan.java
@@ -3,16 +3,14 @@ package by.alex.mobile_operator.entity.plan;
 import by.alex.mobile_operator.entity.plan.enums.AddOn;
 import by.alex.mobile_operator.entity.user.User;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @SuperBuilder
 public abstract class Plan {
-    private Integer id;
+    private final Integer id;
     private String name;
     private Double subscriptionFee;
     private List<AddOn> addOns;
